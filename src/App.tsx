93b8ba5d5bbcce7fdbdd8712c3bcf01fc5de9a62
hussenk <hussen199710@yahoo.com>
2022-08-01 16:58:10 +0200

    move nav tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Page } from "./Page";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <nav>
           <ul>
             <li>
@@ -17,26 +17,26 @@ function App() {
               <Link to={"contact_us"}>contact us</Link>
             </li>
           </ul>
-          <div>
-            <Routes>
-              <Route
-                path="about"
-                element={<Page text={"this page about "} title={"About"} />}
-              ></Route>
-              <Route
-                path="home"
-                element={<Page text={"this page home "} title={"Home"} />}
-              ></Route>
-              <Route
-                path="contact_us"
-                element={
-                  <Page text={"this page contact us "} title={"Contact Us"} />
-                }
-              ></Route>
-            </Routes>
-          </div>
         </nav>
-      </Router>
+        <div>
+          <Routes>
+            <Route
+              path="about"
+              element={<Page text={"this page about "} title={"About"} />}
+            ></Route>
+            <Route
+              path="home"
+              element={<Page text={"this page home "} title={"Home"} />}
+            ></Route>
+            <Route
+              path="contact_us"
+              element={
+                <Page text={"this page contact us "} title={"Contact Us"} />
+              }
+            ></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
