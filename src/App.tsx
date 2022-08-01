9815bd1595d4ef51f00141fec7fa7af8e6229125
hussenk <hussen199710@yahoo.com>
2022-08-01 16:51:26 +0200

    add example how to use routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Page } from "./Page";
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to={"about"}>about</Link>
+            </li>
+            <li>
+              <Link to={"home"}>home</Link>
+            </li>
+            <li>
+              <Link to={"contact_us"}>contact us</Link>
+            </li>
+          </ul>
+          <div>
+            <Routes>
+              <Route
+                path="about"
+                element={<Page text={"this page about "} title={"About"} />}
+              ></Route>
+              <Route
+                path="home"
+                element={<Page text={"this page home "} title={"Home"} />}
+              ></Route>
+              <Route
+                path="contact_us"
+                element={
+                  <Page text={"this page contact us "} title={"Contact Us"} />
+                }
+              ></Route>
+            </Routes>
+          </div>
+        </nav>
+      </Router>
     </div>
   );
 }
